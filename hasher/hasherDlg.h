b8d03ea6833e75c8e5f3f42da81e9ff3a873ae0e
Jan Hodouš <rhscz@post.cz>
2020-03-09 00:13:16 +0100

    small update. fix major issues.

---
+++ hasher/hasherDlg.h
@@ -0,0 +1,77 @@
+
+// hasher2Dlg.h : header file
+//
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+#include <openssl/whrlpool.h>
+#include "afxdialogex.h"
+#include "afxwin.h"
+#include <thread>
+#include <fstream>
+using namespace std;
+#pragma once
+#define bufferSize 10
+#define WM_SHOWPAGE WM_APP+2
+// Chasher2Dlg dialog
+class ChasherDlg : public CDialog
+{
+// Construction
+public:
+	ChasherDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HASHER2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+	
+public:
+	afx_msg void OnBnClickedButton1();
+//	afx_msg void OnCbnSelchangeCombo1();
+	afx_msg void OnBnClickedExit();
+	unsigned char input2[1000] = {};
+	int hash=0;
+	LPSTR input = new char[1000];
+	CString combo;
+	CString buffer, stringout;
+	char* hex = {};
+	unsigned char res[WHIRLPOOL_DIGEST_LENGTH *4] = {};
+	char result[WHIRLPOOL_DIGEST_LENGTH * 4] = {};
+	BOOL retu = 0;
+	HANDLE thread1 = NULL;
+	CFont Font;
+	CButton* MD5check = {};
+	CButton* SHA256check = {};
+	CButton* SHA512check = {};
+	CButton* WHIRLcheck = {};
+	afx_msg void OnBnClickedRadio1();
+	afx_msg void OnBnClickedRadio2();
+	CRichEditCtrl* input_1 = {};
+	CMFCEditBrowseCtrl* input_2 = {};
+	int file_descript = 0;
+	fstream filex;
+	unsigned long file_size = 0;
+	char* file_buffer = {};
+	int hashlen;
+	int ChkBox1;
+	int ChkBox2;
+	int ChkBox3;
+	int ChkBox4;
+	char buf[1024 * 500] = {};
+	MD5_CTX md5Context;
+	SHA256_CTX sha256Context;
+	SHA512_CTX sha512Context;
+	WHIRLPOOL_CTX whirlContext;
+};
---
+++ hasher/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by hasher.rc
+//
+#define IDD_HASHER2_DIALOG              102
+#define IDR_ICON                        128
+#define IDC_BUTTON1                     1000
+#define IDC_INPUT                       1001
+#define IDC_COMBO1                      1002
+#define IDC_MD5OUT                      1003
+#define IDC_SHA512OUT                   1004
+#define IDC_SHA256OUT                   1005
+#define ID_EXIT                         1006
+#define IDC_WHIRLOUT                    1007
+#define IDC_MD5CHECK                    1008
+#define IDC_SHA256CHECK                 1009
+#define IDC_SHA512CHECK                 1010
+#define IDC_WHIRLCHECK                  1011
+#define IDC_RADIO1                      1012
+#define IDC_RADIO2                      1013
+#define IDC_INPUT2                      1019
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
